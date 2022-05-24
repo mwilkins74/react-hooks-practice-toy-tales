660c656db02c6c796a95781517f11aac5e10c22a
Mark <markwilkins86@gmail.com>
2022-05-24 09:54:39 -0400

    commit tree 208eba094ce92c2aee613f0e8641d587c17a2025

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  function [toys, setToys] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/toys')
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+  })
   return (
     <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
   );
